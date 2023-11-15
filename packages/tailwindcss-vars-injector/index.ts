f7bfc274374d0b312f5c41e9199f28a3b03463f1
cotontigeh <cotontigeh@gmail.com>
2023-11-15 14:28:30 +0100

    First release

--- packages/tailwindcss-vars-injector/index.ts
+++ packages/tailwindcss-vars-injector/index.ts
@@ -1,11 +1,9 @@
-// import { ThemeConfig } from "tailwindcss/types/config";
-// export type ThemeKey = Array<keyof Omit<ThemeConfig, "extend">>;
 import { CSSRuleObject } from "tailwindcss/types/config";
 import { defaultConfig } from "./defaultConfig";
 import { TailwindcssVarsInjectorConfig } from "./types";
 
-const flattenColorPalette =
-  require("tailwindcss/lib/util/flattenColorPalette").default;
+// const flattenColorPalette =
+//   require("tailwindcss/lib/util/flattenColorPalette").default;
 
 export function tailwindcssVarsInjector(
   customConfig: TailwindcssVarsInjectorConfig,
@@ -19,19 +17,20 @@ export function tailwindcssVarsInjector(
     addBase: (base: CSSRuleObject | CSSRuleObject[]) => void;
     theme: (path: string, defaultValue?: unknown) => string;
   }) {
-    const colorEntries = Object.entries(
-      flattenColorPalette(theme("colors")),
-    ).map(([key, val]) => [`--${config.prefix}-${key}`, val]);
+    console.log("prout", addBase, theme, config);
+    // const colorEntries = Object.entries(
+    //   flattenColorPalette(theme("colors")),
+    // ).map(([key, val]) => [`--${config.prefix}-${key}`, val]);
 
-    const blurEntries = Object.entries(theme("blur")).map(([key, val]) => [
-      `--${config.prefix}-blur-${key}`,
-      val,
-    ]);
+    // const blurEntries = Object.entries(theme("blur")).map(([key, val]) => [
+    //   `--${config.prefix}-blur-${key}`,
+    //   val,
+    // ]);
 
-    const newVars = Object.fromEntries([...colorEntries, ...blurEntries]);
+    // const newVars = Object.fromEntries([...colorEntries, ...blurEntries]);
 
-    addBase({
-      ":root": newVars,
-    });
+    // addBase({
+    //   ":root": newVars,
+    // });
   };
 }
